5df0b68d3db077e627a753e14e7cb8556bbf71cf
Daniel Ramirez <danielramirez.wm@gmail.com>
2023-02-10 14:11:05 -0500

    fix: revert audit

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,6 +2,10 @@ import React, { useRef } from 'react';
 import '../styles/App.css';
 import playButton from '../assets/images/icons/play_button.png';
 import featuredVideo from '../assets/videos/featured.mp4';
+import leftButtonActive from '../assets/images/icons/left_active.png';
+import rightButtonActive from '../assets/images/icons/right_active.png';
+import leftButtonInactive from '../assets/images/icons/left_inactive.png';
+import rightButtonInactive from '../assets/images/icons/right_inactive.png';
 
 function App() {
 
@@ -30,6 +34,97 @@ function App() {
         </div>
         <img src={playButton} alt="play button" onClick={handleVideo} />
       </div>
+
+
+
+      <div className="movies-menu-container">
+        <div className="menu-container">
+          <span className="menu-item selected">Today</span>
+          <span className="menu-item separator">/</span>
+          <span className="menu-item">This week</span>
+          <span className="menu-item separator">/</span>
+          <span className="menu-item">Last 30 days</span>
+        </div>
+      </div>
+
+      <div className="movies-carousel">
+        <div className="movies-container">
+
+          <div className="movie">
+            <div className="movie-cover">
+              <img src="https://picsum.photos/164/240" alt="movie cover" />
+            </div>
+            <div className="movie-info">
+              <p className="movie-genre">Genre, Year</p>
+              <p className="movie-name">Movie name</p>
+            </div>
+          </div>
+
+          <div className="movie">
+            <div className="movie-cover">
+              <img src="https://picsum.photos/164/240" alt="movie cover" />
+            </div>
+            <div className="movie-info">
+              <p className="movie-genre">Genre, Year</p>
+              <p className="movie-name">Movie name</p>
+            </div>
+          </div>
+
+          <div className="movie">
+            <div className="movie-cover">
+              <img src="https://picsum.photos/164/240" alt="movie cover" />
+            </div>
+            <div className="movie-info">
+              <p className="movie-genre">Genre, Year</p>
+              <p className="movie-name">Movie name</p>
+            </div>
+          </div>
+
+          <div className="movie">
+            <div className="movie-cover">
+              <img src="https://picsum.photos/164/240" alt="movie cover" />
+            </div>
+            <div className="movie-info">
+              <p className="movie-genre">Genre, Year</p>
+              <p className="movie-name">Movie name</p>
+            </div>
+          </div>
+
+          <div className="movie">
+            <div className="movie-cover">
+              <img src="https://picsum.photos/164/240" alt="movie cover" />
+            </div>
+            <div className="movie-info">
+              <p className="movie-genre">Genre, Year</p>
+              <p className="movie-name">Movie name</p>
+            </div>
+          </div>
+
+          <div className="movie">
+            <div className="movie-cover">
+              <img src="https://picsum.photos/164/240" alt="movie cover" />
+            </div>
+            <div className="movie-info">
+              <p className="movie-genre">Genre, Year</p>
+              <p className="movie-name">Movie name</p>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="movies-carousel-header">
+          <p className="header-title">Action & Drama Movies</p>
+          <div className="movies-carousel-buttons-container">
+            <img src={leftButtonInactive} alt="Left arrow" />
+            <img src={rightButtonActive} alt="Right arrow" />
+          </div>
+          <div className="line"></div>
+          <p className="view">VIEW ALL +</p>
+        </div>
+
+
+      </div>
+
     </div>
   );
 }
